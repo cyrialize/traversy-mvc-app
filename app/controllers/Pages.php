c48ce692a0ad4f24d3195840c2aea724bcf5f5dc
cyrialize <jonathancyr@fastmail.com>
2021-03-24 13:24:17 -0400

    Completed lesson 21

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -17,6 +17,9 @@ class Pages extends Controller
 
     public function about()
     {
-        $this->view('pages/about');
+        $data = [
+            'title' => 'About Us'
+        ];
+        $this->view('pages/about', $data);
     }
 }
