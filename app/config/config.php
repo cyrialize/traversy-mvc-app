bda5d687b9818534111fe529a34cd77cd3e47a87
cyrialize <jonathancyr@fastmail.com>
2021-03-25 16:23:49 -0400

    Completed lesson 25 the clean up

--- app/config/config.php
+++ app/config/config.php
@@ -1,18 +1,16 @@
 <?php
 
-$config = parse_ini_file('config.ini');
-
 // DB Params
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', $config['DB_PASS']);
-define('DB_NAME', 'tvmc');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
 
 // App Root 
 define('APPROOT', dirname(dirname(__FILE__)));
 
 // URL Root
-define('URLROOT', 'http://localhost:8000');
+define('URLROOT', '');
 
 // Site Name
-define('SITENAME', 'TraversyMVC');
+define('SITENAME', '');
