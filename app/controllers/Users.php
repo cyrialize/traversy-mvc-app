30ae9c9cadd811f036550131f70f8609a567c100
cyrialize <jonathancyr@fastmail.com>
2021-04-02 13:46:18 -0400

    Completed lesson 29

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -27,4 +27,21 @@ class Users extends Controller
             $this->view('users/register', $data);
         }
     }
+
+    public function login()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        } else {
+
+            $data = [
+                'email' => '',
+                'password' => '',
+                'email_err' => '',
+                'password_err' => '',
+            ];
+
+            $this->view('users/login', $data);
+        }
+    }
 }
