0b458372c3c6629c67ff7edf4e490281dd08fc8a
cyrialize <jonathancyr@fastmail.com>
2021-04-14 16:03:21 -0400

    completed lesson 30

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -10,6 +10,46 @@ class Users extends Controller
     public function register()
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Sanitize POST data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'confirm_password' => trim($_POST['confirm_password']),
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => ''
+            ];
+
+            if (empty($data['email'])) {
+                $data['email_err'] = 'Please enter email';
+            }
+
+            if (empty($data['name'])) {
+                $data['name_err'] = 'Please enter name';
+            }
+
+            if (empty($data['password'])) {
+                $data['password_err'] = 'Please enter password';
+            } else if (strlen($data['password']) < 6) {
+                $data['password_err'] = 'Password must be at least 6 characters';
+            }
+
+            if (empty($data['confirm_password'])) {
+                $data['confirm_password_err'] = 'Please confirm password';
+            } else if ($data['password'] != $data['confirm_password']) {
+                $data['password_err'] = 'Passwords do not match';
+            }
+
+            if (empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])) {
+                die('SUCCESS');
+            } else {
+                // Load view with errors 
+                $this->view('users/register', $data);
+            }
 
         } else {
 
@@ -31,6 +71,31 @@ class Users extends Controller
     public function login()
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            // Sanitize POST data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'email_err' => '',
+                'password_err' => ''
+            ];
+
+            if (empty($data['email'])) {
+                $data['email_err'] = 'Please enter email';
+            }
+
+            if (empty($data['password'])) {
+                $data['password_err'] = 'Please enter password';
+            }
+
+            if (empty($data['email_err']) && empty($data['password_err'])) {
+                die('SUCCESS');
+            } else {
+                // Load view with errors 
+                $this->view('users/login', $data);
+            }
 
         } else {
 
