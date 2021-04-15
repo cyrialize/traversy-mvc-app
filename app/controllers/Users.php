ad9bb88cb183b4563e066d94ec32b9e349447fed
cyrialize <jonathancyr@fastmail.com>
2021-04-15 16:38:22 -0400

    Completed lesson 33

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -50,6 +50,7 @@ class Users extends Controller
                 $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
                 
                 if ($this->userModel->register($data)) {
+                    flash('register_success', 'You are registered and can log in');
                     redirect('users/login');
                 } else {
                     die('Something went wrong');
