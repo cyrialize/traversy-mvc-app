699df73a217cc6dca2f8518d2462e0fd3c17034f
cyrialize <jonathancyr@fastmail.com>
2021-04-15 16:39:20 -0400

    Removed not needed if statements in session_helper, we do not need to unset because we overwrite

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -9,13 +9,6 @@ function flash($name = '', $message = '', $class = 'alert alert-success')
     if (!empty($name)) {
 
         if (!empty($message) && empty($_SESSION[$name])) {
-            if (!empty($_SESSION[$name])) {
-                unset($_SESSION[$name]);
-            }
-    
-            if (!empty($_SESSION[$name . '_class'])) {
-                unset($_SESSION[$name . '_class']);
-            }
     
             $_SESSION[$name] = $message;
             $_SESSION[$name . '_class'] = $class;
